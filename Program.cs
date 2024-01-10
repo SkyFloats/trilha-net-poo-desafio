b8669398ab7750292505f11164d19f34f394e80b
SkyFloats <dan17172017@gmail.com>
2024-01-09 23:22:12 -0300

    Desafio de POO Feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "912345678", modelo:"Nokia Tijolão", imei:"987654321", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Mobile Legends");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "12121212", modelo:"Iphone 30", imei:"159478236", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Shein App");
